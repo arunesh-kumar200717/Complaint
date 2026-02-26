832574d90ea036750a5b72e90c144d2ca87a0dfa
Arunesh kumar C <aruneshk075@gmail.com>
2026-02-26 20:29:27 +0530

    Create userRoutes.js file in backend/src/routes/ directory

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,34 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { authenticate } = require('../../middlewares/auth');
+const userController = require('../controllers/userController');
+const { authenticate } = require('../middleware/auth');
 
-// Mock User Controller (you should replace this with actual logic)
-const userController = {
-    getAllUsers: (req, res) => {
-        // Logic to get all users
-        res.send('Get all users');
-    },
-    getUserById: (req, res) => {
-        // Logic to get user by ID
-        const { id } = req.params;
-        res.send(`Get user with ID: ${id}`);
-    },
-    updateUser: (req, res) => {
-        // Logic to update user
-        const { id } = req.params;
-        res.send(`Update user with ID: ${id}`);
-    },
-    deleteUser: (req, res) => {
-        // Logic to delete user
-        const { id } = req.params;
-        res.send(`Delete user with ID: ${id}`);
-    }
-};
-
-// Routes
 router.get('/', authenticate, userController.getAllUsers);
 router.get('/:id', authenticate, userController.getUserById);
-router.put('/:id', authenticate, userController.updateUser);
-router.delete('/:id', authenticate, userController.deleteUser);
+router.put('/:id', authenticate, userController.updateUserById);
+router.delete('/:id', authenticate, userController.deleteUserById);
 
 module.exports = router;
